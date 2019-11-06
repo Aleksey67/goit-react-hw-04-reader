6822b503c3260914a272b664465e7120a61aee41
Aleksey Baydyk <lehalevan@gmail.com>
2019-11-06 20:12:40 +0200

    controls disable buttons

--- src/components/Reader/Controls/Controls.jsx
+++ src/components/Reader/Controls/Controls.jsx
@@ -2,10 +2,11 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import styles from './Controls.module.css';
 
-const Controls = ({ onPageChange }) => (
+const Controls = ({ onPageChange, page, items }) => (
   <section className={styles.controls}>
     <button
       type="button"
+      disabled={page === 0}
       className={styles.button}
       onClick={() => onPageChange(-1)}
     >
@@ -13,6 +14,7 @@ const Controls = ({ onPageChange }) => (
     </button>
     <button
       type="button"
+      disabled={page === items.length - 1}
       className={styles.button}
       onClick={() => onPageChange(1)}
     >
@@ -23,6 +25,14 @@ const Controls = ({ onPageChange }) => (
 
 Controls.propTypes = {
   onPageChange: PropTypes.func.isRequired,
+  page: PropTypes.number.isRequired,
+  items: PropTypes.arrayOf(
+    PropTypes.exact({
+      id: PropTypes.string,
+      title: PropTypes.string,
+      text: PropTypes.string,
+    }),
+  ).isRequired,
 };
 
 export default Controls;
