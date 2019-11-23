8d80f5970fb092c707c1a651f85d551723f646e8
Aleksey Baydyk <lehalevan@gmail.com>
2019-11-23 14:32:11 +0200

    Updates

--- precache-manifest.a826dd4708361e7047efeca12579ad7a.js
+++ precache-manifest.a826dd4708361e7047efeca12579ad7a.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "b5f214b3c44bc34e44a94972fcc88026",
+    "revision": "e49475d73f3f5fb67bf181342454a4cf",
     "url": "/goit-react-hw-04-reader/index.html"
   },
   {
-    "revision": "e79146ece969485af009",
+    "revision": "42dbfafe13b3441b87c6",
     "url": "/goit-react-hw-04-reader/static/css/main.c3add8ac.chunk.css"
   },
   {
@@ -12,8 +12,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/goit-react-hw-04-reader/static/js/2.2079ae8f.chunk.js"
   },
   {
-    "revision": "e79146ece969485af009",
-    "url": "/goit-react-hw-04-reader/static/js/main.f2c031a9.chunk.js"
+    "revision": "42dbfafe13b3441b87c6",
+    "url": "/goit-react-hw-04-reader/static/js/main.dfc1b92a.chunk.js"
   },
   {
     "revision": "1007062e66537ff85ac4",
