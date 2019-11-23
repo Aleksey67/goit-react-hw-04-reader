1eb7850cd4efaa9c0f75aa8542a11bcb6682c35f
Aleksey Baydyk <lehalevan@gmail.com>
2019-11-23 14:28:00 +0200

    Updates

---
+++ precache-manifest.8a9ad2d70f4fbe4cf45f2e9fe2348f6a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b5f214b3c44bc34e44a94972fcc88026",
+    "url": "/goit-react-hw-04-reader/index.html"
+  },
+  {
+    "revision": "e79146ece969485af009",
+    "url": "/goit-react-hw-04-reader/static/css/main.c3add8ac.chunk.css"
+  },
+  {
+    "revision": "03a384f4535f3d50da4b",
+    "url": "/goit-react-hw-04-reader/static/js/2.2079ae8f.chunk.js"
+  },
+  {
+    "revision": "e79146ece969485af009",
+    "url": "/goit-react-hw-04-reader/static/js/main.f2c031a9.chunk.js"
+  },
+  {
+    "revision": "1007062e66537ff85ac4",
+    "url": "/goit-react-hw-04-reader/static/js/runtime-main.726f0809.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.ce4c8923752fd6f87f9e20661128e5d2.js"
+  "/goit-react-hw-04-reader/precache-manifest.8a9ad2d70f4fbe4cf45f2e9fe2348f6a.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/goit-react-hw-04-reader/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
