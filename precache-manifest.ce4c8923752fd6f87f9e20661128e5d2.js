ad4fd3ba1d5a42de926c2e0bbc8ceebf2d943cd6
Aleksey Baydyk <lehalevan@gmail.com>
2019-11-23 14:22:21 +0200

    Updates

---
+++ precache-manifest.ce4c8923752fd6f87f9e20661128e5d2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f84a35ef2800bcaed263ce1fcbd9138d",
+    "url": "./index.html"
+  },
+  {
+    "revision": "e79146ece969485af009",
+    "url": "./static/css/main.c3add8ac.chunk.css"
+  },
+  {
+    "revision": "03a384f4535f3d50da4b",
+    "url": "./static/js/2.2079ae8f.chunk.js"
+  },
+  {
+    "revision": "e79146ece969485af009",
+    "url": "./static/js/main.f2c031a9.chunk.js"
+  },
+  {
+    "revision": "212a6ce5d2ae8f1bb11f",
+    "url": "./static/js/runtime-main.42391edd.js"
+  }
+]);
