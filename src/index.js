2ba8f100311488f782d0ffddc8f376c851263299
Aleksey Baydyk <lehalevan@gmail.com>
2019-11-25 18:35:02 +0200

    gh pages fix

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import { HashRouter } from 'react-router-dom';
 import App from './components/App/App';
 
 ReactDOM.render(
-  <HashRouter basename="/goit-react-hw-04-reader">
+  <HashRouter>
     <App />
   </HashRouter>,
   document.querySelector('#root'),
